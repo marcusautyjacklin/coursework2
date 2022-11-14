54607e754a232118e48140b134b2f592f3b3c4bb
marcusautyjacklin <74726482+marcusautyjacklin@users.noreply.github.com>
2022-11-14 13:49:37 +0000

    Added file read and class generation for cities

--- utils.py
+++ utils.py
@@ -1,4 +1,20 @@
 from cities import City, CityCollection
+from pathlib import Path
+import csv
 
 def read_attendees_file(filepath: Path) -> CityCollection:
-    raise NotImplementedError
+
+    filepath = Path("attendee_locations.csv")
+
+    file = open(filepath)
+    csv_file = csv.reader(file)
+    next(csv_file)
+    variables = vars()
+    cities = []
+
+    for row in csv_file:
+        name = row[3]
+        name.replace(" ","_")
+        name = name.lower()
+        print(type(name))
+        variables[name] = City(str(row[3]), str(row[1]), int(row[0]), float(row[4]), float(row[5]))
