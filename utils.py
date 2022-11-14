f2b9d481e2f0953e7288ea87416acbc60e64e54b
marcusautyjacklin <74726482+marcusautyjacklin@users.noreply.github.com>
2022-11-14 15:35:53 +0000

    Adjusting output to CityCollection class and notes

--- utils.py
+++ utils.py
@@ -2,10 +2,6 @@ from cities import City, CityCollection
 from pathlib import Path
 import csv
 
-
-filepath = Path("attendee_locations.csv")
-
-
 def read_attendees_file(filepath: Path) -> CityCollection:
 
     file = open(filepath)
@@ -19,14 +15,21 @@ def read_attendees_file(filepath: Path) -> CityCollection:
         name = row[3]
         name = name.replace(" ","_")
         name = name.lower()
+
         # Create dictionary with city name as the key and value which corresponds to the index of city_list holding the City class for this city.
         cities_dict[name] = csv_file.line_num-2
+
         # Append class to list with for corresponding row in input csv. Dictionary has associated index for each city.
+        # Usage: print(city_list[cities_dict["algiers"]])
         city_list.append(City(str(row[3]), str(row[1]), int(row[0]), float(row[4]), float(row[5])))
 
-    return cities_dict, city_list
+    # Create city collection class from list of city classes
+    collection = CityCollection(city_list)
 
-cities_dict, city_list = read_attendees_file(Path("attendee_locations.csv"))
-print(city_list[cities_dict["algiers"]])
+    return collection, cities_dict
+
+
+filepath = Path("attendee_locations.csv")
+collection, cities_dict = read_attendees_file(Path("attendee_locations.csv"))
 
 
