ce028e82a0c28ea0e925c96d145b1fa5a3cb997b
marcusautyjacklin <74726482+marcusautyjacklin@users.noreply.github.com>
2022-11-15 09:59:34 +0000

    defined various tests to be performed

---
+++ test_cities.py
@@ -0,0 +1,46 @@
+## City class tests ##
+
+def test_publicTransport_travel():
+    raise NotImplementedError
+
+def test_shortHaul_travel():
+    raise NotImplementedError
+
+def test_longHaul_travel():
+    raise NotImplementedError
+
+def test_publicTransport_co2():
+    raise NotImplementedError
+
+def test_shortHaul_co2():
+    raise NotImplementedError
+
+def test_longHaul_co2():
+    raise NotImplementedError
+
+## CityCollection class tests ##
+
+# Info about collection #  
+
+def test_countries():
+    raise NotImplementedError
+
+def test_attendees():
+    raise NotImplementedError
+
+# Info specifc to host-city # 
+
+def test_total_distance():
+    raise NotImplementedError
+
+def test_travel_byCountry():
+    raise NotImplementedError
+
+def tets_co2_byCountry():
+    raise NotImplementedError
+
+def test_total_co2():
+    raise NotImplementedError
+
+def test_sortedByEmissions():
+    raise NotImplementedError
