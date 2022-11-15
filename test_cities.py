cf67687d968cd6bde4e77f70d16e72537fece732
marcusautyjacklin <74726482+marcusautyjacklin@users.noreply.github.com>
2022-11-15 11:13:10 +0000

    added city class tests

--- test_cities.py
+++ test_cities.py
@@ -1,22 +1,61 @@
+import pytest
+from utils import read_attendees_file
+from cities import City, CityCollection
+from pathlib import Path
+import csv
+
 ## City class tests ##
 
 def test_publicTransport_travel():
-    raise NotImplementedError
+    london = City('London', 'United Kingdom', 117, 51.5073219, -0.1276474)
+    paris = City('Paris', 'France', 173, 48.8566101, 2.3514992)
+    # assert round(london.distance_to(paris),2) == 343.52
+    assert london.distance_to(paris) == 343.51651914890107
+
+    # assert round(city_collection.cities[cities_dict["london"]].distance_to(city_collection.cities[cities_dict["paris"]]),2) == 343.52
+    # raise NotImplementedError
 
 def test_shortHaul_travel():
-    raise NotImplementedError
+    toronto = City('Toronto', 'Canada', 89,43.653963, -79.387207)
+    los_angeles = City('Los Angeles', 'United States', 298, 34.0536909, -118.2427666)
+    # assert round(toronto.distance_to(los_angeles),2) == 3493.91
+    assert toronto.distance_to(los_angeles) == 3493.908413422292
+
+    # assert round(city_collection.cities[cities_dict["toronto"]].distance_to(city_collection.cities[cities_dict["los_angeles"]]),2) == 3493.91
+    # raise NotImplementedError
 
 def test_longHaul_travel():
-    raise NotImplementedError
+    beijing = City('Beijing', 'China', 950, 39.906217, 116.3912757)
+    san_francisco = City('San Francisco', 'United States', 64, 37.7792808, -122.4192363)
+    # assert round(beijing.distance_to(san_francisco),2) == 9503.49
+    assert beijing.distance_to(san_francisco) == 9503.485826688868
+
+    # assert round(city_collection.cities[cities_dict["beijing"]].distance_to(city_collection.cities[cities_dict["san_francisco"]]),2) == 9503.49
+    # raise NotImplementedError
 
 def test_publicTransport_co2():
-    raise NotImplementedError
+    london = City('London', 'United Kingdom', 117, 51.5073219, -0.1276474)
+    paris = City('Paris', 'France', 173, 48.8566101, 2.3514992)
+    # assert round(london.co2_to(paris),2) == 8038286.55
+    assert london.co2_to(paris) == 8038286.548084285
+
+    # raise NotImplementedError
 
 def test_shortHaul_co2():
-    raise NotImplementedError
+    toronto = City('Toronto', 'Canada', 89,43.653963, -79.387207)
+    los_angeles = City('Los Angeles', 'United States', 298, 34.0536909, -118.2427666)
+    # assert round(toronto.co2_to(los_angeles),2) == 73289462.12
+    assert toronto.co2_to(los_angeles) == 73289462.198646
+
+    # raise NotImplementedError
 
 def test_longHaul_co2():
-    raise NotImplementedError
+    beijing = City('Beijing', 'China', 950, 39.906217, 116.3912757)
+    san_francisco = City('San Francisco', 'United States', 64, 37.7792808, -122.4192363)
+    # assert round(beijing.co2_to(san_francisco),2) == 2280993460.61
+    assert beijing.co2_to(san_francisco) == 2280993460.6063275
+
+    # raise NotImplementedError
 
 ## CityCollection class tests ##
 
